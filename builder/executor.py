0b9b3c8644eb971cc7d8f93e43dde34dcbeda339
gerva <gervaland@hotmail.com>
2011-11-27 21:06:31 +0100

    fixes for timeout; updated tests

--- builder/executor.py
+++ builder/executor.py
@@ -2,7 +2,7 @@ import sys
 import os
 import subprocess
 import tempfile
-import datetime 
+import datetime
 import log
 log = log.logger(__name__)
 
@@ -16,7 +16,7 @@ class Executor(object):
        self.err_fd = None
        self.stdout_r = None
        self.stderr_r = None
-       # command 
+       # command
        self.proc = None
        self.cmd = None
        self.cwd = None
@@ -54,7 +54,7 @@ class Executor(object):
                raise ExecutorError(msg)
        except TypeError, e:
          if cwd is not None:
-             log.error('cwd error: expected string')
+             log.debug('cwd error: expected string')
              raise
        self.cwd = cwd
 
@@ -65,25 +65,32 @@ class Executor(object):
 
    def _set_timeout(self, timeout):
        if not isinstance(timeout, (int, float)) and timeout is not None:
-           raise TypeError('timeout should be None, int or float') 
+           raise TypeError('timeout should be None, int or float')
        try:
            self.timeout = datetime.datetime.now() + datetime.timedelta(0,timeout)
        except TypeError:
-           pass
+           self.timeout = None
 
    def sanitise_input(self, cmd, cwd, env, timeout):
+       # cmd, cwd, env and timeout need to be reset
+       # before executing a new comand
+       self.cmd = None
+       self.cwd = None
+       self.env = None
+       self.timeout = None
        # check cmd is a list
        self._set_cmd(cmd)
        # ensuring working directory exists or is None
        self._set_cwd(cwd)
        # check env is a dict
        self._set_env(env)
-       # check timeout 
+       # check timeout
        self._set_timeout(timeout)
        log.debug('executing:')
        log.debug('cmd: {0}'.format(' '.join(self.cmd)))
        log.debug('cwd: {0}'.format(self.cwd))
        log.debug('env: {0}'.format(self.env))
+       log.debug('timeout: {0}'.format(self.timeout))
 
    def execute(self, cmd, cwd, env, timeout):
        self.sanitise_input(cmd, cwd, env, timeout)
@@ -91,13 +98,12 @@ class Executor(object):
        self.proc = subprocess.Popen(self.cmd, cwd=self.cwd, env=self.env,
                                stdout = self.out_fd,
                                stderr = self.err_fd)
-       self.start_time = datetime.datetime.now()
-       return self.wait_completion_or_timeout()
-
-   def wait_completion_or_timeout(self):
-       while self.proc.poll() is None or self.time_to_stop():
-               self.write_process_out_to_log()
+       self.wait_completion_or_timeout()
        # log all remaining lines from out and err files
+       self.log_last_lines()
+       return self.proc.returncode
+
+   def log_last_lines(self):
        if self.log_enable:
            out_file_read = open(self.out_fd.name, 'r')
            for line in out_file_read.readlines() :
@@ -105,23 +111,35 @@ class Executor(object):
            err_file_read = open(self.err_fd.name, 'r')
            for line in err_file_read.readlines() :
                log.error(line.strip())
+
+   def got_timeout(self):
+       if self.timeout == None:
+           return False
+       return datetime.datetime.now() >= self.timeout
+
+   def wait_completion_or_timeout(self):
+       while self.proc.poll() is None:
+           self.write_process_out_to_log()
+           if self.timeout is not None:
+              if self.got_timeout():
+                  log.debug('timeout: terminating the process')
+                  log.debug('now: {0}, timeout {1}'.format(datetime.datetime.now(), self.timeout ))
+                  self.proc.terminate()
        log.debug('exit code: {0}'.format(self.proc.returncode))
        return self.proc.returncode
 
-   def time_to_stop(self):
-       try:
-           return datetime.datetime.now()  >= self.timeout
-       except TypeError:
-           return False
-
    def write_process_out_to_log(self):
+       if self.timeout is not None:
+           now = datetime.datetime.now()
+           diff = self.timeout < now
+           #print 't - now = {0} - {1} = {2}'.format(self.timeout, datetime.datetime.now(), diff)
        if not self.log_enable:
            stdout = self.stdout_r.readline().rstrip()
-           if stdout != '': 
+           if stdout != '':
                log.debug('stdout: {0}'.format(stdout))
        stderr = self.stderr_r.readline().rstrip()
-       if stderr != '': 
-           log.error('stderr: {0}'.format(stderr))
+       if stderr != '':
+           log.debug('stderr: {0}'.format(stderr))
 
 class ExecutorError(Exception):
    def __init__(self, value):
