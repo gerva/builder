4760394bbb39783ef924065dfad484ec8a1b1a98
gerva <gervaland@hotmail.com>
2011-11-27 21:06:51 +0100

    updated tests

--- test/test_executor.py
+++ test/test_executor.py
@@ -16,19 +16,33 @@ class testExecutor(unittest.TestCase):
         """
         cmd_1 = [ 'cd', '..' ]
         cmd_2 = [ 'ls', 'A_]1msn1iisn a' ]
+        cmd_3 = [ 'sleep', '3' ]
         self.assertEqual(self.e.execute(cmd_1, cwd=os.path.join('/'), env=None, timeout=None), 0)
         self.assertEqual(self.e.execute(cmd_2, cwd=os.path.join('/'), env=None, timeout=None), 1)
-        self.assertEqual(self.e.execute(['ls'], cwd=None, env=None, timeout=None), 0)
+        self.assertEqual(self.e.execute(cmd_1, cwd=None, env=None, timeout=None), 0)
+        self.assertEqual(self.e.execute(cmd_1, cwd=None, env={'MY_VAR': '/tmp'}, timeout=None), 0)
+        self.assertEqual(self.e.execute(cmd_1, cwd=None, env={'MY_VAR': '/tmp'}, timeout=2), 0)
+        self.assertEqual(self.e.execute(cmd_3, cwd=None, env={'MY_VAR': '/tmp'}, timeout=None), 0)
+        self.assertNotEqual(self.e.execute(cmd_3, cwd=None, env={'MY_VAR': '/tmp'}, timeout=2), 0)
 
     def testBadInputForExecute(self):
         """
         passing to the executor, wrong commands
         """
+        # excute... None
         self.assertRaises(TypeError, lambda: self.e.execute(None, cwd=None, env=None, timeout=None))
+        # wrong cmd type (cmd expects a list)
         self.assertRaises(TypeError, lambda: self.e.execute(cmd='ls', cwd=None, env=None, timeout=None))
-        self.assertRaises(TypeError, lambda: self.e.execute(cmd=['ls'], cwd=['/nonexisitingdir'], env=None, timeout=None))
-        self.assertRaises(ex.ExecutorError, lambda: self.e.execute(cmd=['ls'], cwd='/nonexisitingdir', env=None, timeout=None))
         self.assertRaises(TypeError, lambda: self.e.execute(cmd=1, cwd='aaaa', env=None, timeout=None))
         self.assertRaises(TypeError, lambda: self.e.execute(None, cwd='aaaa', env=None, timeout=None))
-        self.assertRaises(ex.ExecutorError, lambda: self.e.execute(['cd'], cwd='aaaa', env=None, timeout=None))
+        # cwd should be a string... passing a list
+        self.assertRaises(TypeError, lambda: self.e.execute(cmd=['ls'], cwd=['/nonexisitingdir'], env=None, timeout=None))
+        # cwd does not exist 
+        self.assertRaises(ex.ExecutorError, lambda: self.e.execute(cmd=['ls'], cwd='/nonexisitingdir', env=None, timeout=None))
+        # env 
+        self.assertRaises(TypeError, lambda: self.e.execute(['cd'], cwd=None, env=list(), timeout=None))
+        self.assertRaises(TypeError, lambda: self.e.execute(['cd'], cwd=None, env=1, timeout=None))
+        self.assertRaises(TypeError, lambda: self.e.execute(['cd'], cwd=None, env='', timeout=None))
+        # timeout
+        self.assertRaises(TypeError, lambda: self.e.execute(['cd'], cwd=None, env='', timeout=''))
 
