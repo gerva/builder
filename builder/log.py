aebeecaf0adadfaf39f537eed9a5393864eb34c5
gerva <gervaland@hotmail.com>
2011-11-27 14:55:47 +0100

    moved content of conf/common/log in builder/log.py

--- builder/log.py
+++ builder/log.py
@@ -1,5 +1,36 @@
 import logging
 import logging.config
 
-def CustomLogger(file_config):
-    pass
+
+cfg = {
+  'version': 1,
+  'filters': {},
+  'formatters': {
+      'short': {'format': '%(message)s'},
+      'long': { 'format': '%(asctime)s %(levelname)-8s %(name)-15s %(message)s',
+                'datefmt': '%Y-%m-%d %H:%M:%S'},
+  },
+  'handlers': {
+      'console': {
+          'class': 'logging.StreamHandler',
+          'formatter': 'short',
+          'level': 'INFO',
+          'stream': 'ext://sys.stdout',
+      },
+      'file': {
+          '()': 'logging.FileHandler',
+          'formatter': 'long',
+          'mode' : 'a',
+          'filename': '/tmp/test.log',
+      },
+  },
+  'root': {
+      'level' : logging.DEBUG,
+      'handlers': ['file', 'console'],
+  },
+}
+
+def logger(logger_name, file_name):
+    cfg['handlers']['file']['filename'] = file_name
+    logging.config.dictConfig(cfg)
+    return logging.getLogger(logger_name)
