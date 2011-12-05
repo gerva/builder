00cb7d4fdddd0d70bdedce379e2bc4057d6ecf5f
gerva <gervaland@hotmail.com>
2011-12-05 16:06:16 +0100

    updated tests for git_interface module

--- test/test_git_interface.py
+++ test/test_git_interface.py
@@ -11,15 +11,22 @@ class testGitInterface(unittest.TestCase):
     """
     def setUp(self):
         self.mirror_dir = tempfile.mkdtemp()
-        print self.mirror_dir
         self.git = git.GitInterface(self.mirror_dir, sys.path[0])
 
     def tearDown(self):
         shutil.rmtree(self.mirror_dir)
 
+    def testGitTag(self):
+        tag = git.GitTag('2510022b75a5d1ceaf977d3d86431b6bbd4d0a1e	refs/tags/test')
+        self.assertEqual(tag.commitId, '2510022b75a5d1ceaf977d3d86431b6bbd4d0a1e')
+        self.assertEqual(tag.name, 'test')
+
     def testMirrorFromLocal(self):
         """
-        testing a mirror clone 
+        testing a mirror clone
         """
         self.git.update_mirror()
         self.assertEqual(os.path.exists(self.mirror_dir), True)
+        self.assertTrue('test' in self.git.get_tag_names('origin'))
+        self.assertTrue('origin' in self.git.get_remotes())
+
