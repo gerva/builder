023054cb9842bdccf3e1a1ec026aa0e3e43b4d70
gerva <gervaland@hotmail.com>
2011-12-07 15:16:58 +0100

    added git_repository (high level git interface) and project

---
+++ builder/git_repository.py
@@ -0,0 +1,31 @@
+import git_interface
+import log_setup
+
+log = log_setup.logger()
+
+class GitRepository(git_interface.GitInterface):
+   def __init__(self, mirror_directory, remote_repository):
+       git_interface.GitInterface.__init__(self, mirror_directory, remote_repository)
+       self.working_directory = None
+
+   def update(self):
+       log.info('updating mirror')
+       self.update_mirror()
+
+   def create_working_copy(self, working_directory, branch='master'):
+       log.info('creating working copy')
+       if not branch in self.get_remote_branch_names('origin'):
+           raise GitRepositoryError('requested branch {0} does not exists'.format(branch))
+       self.working_directory = working_directory
+       try:
+           self.create_working_directory(self.working_directory)
+           self.branch(branch, 'origin/{0}'.format(branch))
+       except git_interface.GitError, e:
+           raise GitRepositoryError('error creating the working directory: {0}'.format(e))
+
+class GitRepositoryError(Exception):
+   def __init__(self, value):
+       self.value = value
+
+   def __str__(self):
+       return repr(self.value)
---
+++ builder/project.py
@@ -0,0 +1,63 @@
+import os
+import shutil
+import tempfile
+import log_setup
+import git_interface as git
+import builder.process as process
+
+log = log_setup.logger()
+
+class Project(object):
+    """
+    describes a Project
+    """
+    def __init__(self, name, git_interface):
+        self.name = name
+        self.git = git_interface
+        self.working_dir = tempfile.mkdtemp()
+    
+    def checkout(self, branch):
+        """
+        creates a working copy
+        """
+        try:
+            self.git.update_mirror()
+            self.git.create_working_directory(self.working_dir)
+            self.git.branch(branch, 'origin/{0}'.format(branch))
+        except git.GitError, e:
+            raise ProjectError('checkout failed: {0}'.format(e))
+
+    def build(self, cmd, env):
+        build = process.ExternalProcess()
+        return build.execute(cmd=cmd, cwd=self.working_dir, env=env, timeout=None)
+    
+    def tag(self, tag_name):
+        """
+        adds a tag in git
+        """
+        pass
+
+    def notify(self):
+        """
+        sends out notifications
+        """
+        pass
+
+    def copy_working_dir_to(self, path):
+        """
+        creates a copy of the working dir; 
+        working dir is destroyed at the end of the process
+        create a copy if you want to preserve the working copy
+        """ 
+        pass
+
+    def close(self):
+        if os.path.exists(self.working_dir):
+            shutil.rmtree(self.working_dir)
+
+class ProjectError(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
